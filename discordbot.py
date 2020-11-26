0b9d141252afa0203932d65fac74322d3eb408b9
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2020-11-26 16:50:18 +0100

    add cmds for temp private channel handling
    uses @bot.command instead of message.content.startswith for the new commands, so some small changes to the existing code are needed apart from adding the new cmds
    
    new commands (ONLY USABLE BY STAFF and if fobot is not muted) are:
    !fobot purge <channelname> <amount of messages to be deleted> --> Deletes up to messages of the specified channel. If no channel is specified it will purge the channel the command was issued in. Amount defaults to 100.
    !fobot copy <channelname> --> Copies the specified channel (up to 10,000 msgs) to "Private_chat_log". If no channel is specified it will copy the channel the command was issued in. Only works in channels that start with "temp_".
    !fobot kill_channel <channelname> --> Copies the specified channel to "Private_chat_log" and deletes it thereafter. If no channel is specified it defaults to the channel the command was issued in. Only works in channels that start with "temp_".
    !fobot create <user> --> Creates a temp. private channel and invites the specified user to it.
    
    new cmds have a help info that can be shown via !fobot help <cmd-name>
    
    also needs 2 entries ("logtarget", the id of the channel we want to store stuff in and "privcat", the id of the Private Channels Category (360564294401916929)) to discordbottoken.py for the cmds copy, create and kill_channel to work properly

--- discordbot.py
+++ discordbot.py
@@ -12,8 +12,10 @@ import json
 import time
 import datetime
 import urllib
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = '!fobot ')
+client = bot
 
 muted = False
 game = discord.Game(name='Liquipedia', url='https://liquipedia.net', type=1)
@@ -509,6 +511,7 @@ async def on_message(message):
 			elif message.content == '!fobot mute':
 				muted = True
 				await message.channel.send(embed=discord.Embed(colour=discord.Colour(0x00ff00), description='*Bot is muted now!*'))
+			else: await bot.process_commands(message)
 		if message.content == '!fobot unmute':
 			muted = False
 			await message.channel.send(embed=discord.Embed(colour=discord.Colour(0x00ff00), description='*Bot is unmuted now!*'))
@@ -601,4 +604,105 @@ async def on_message(message):
 					if result != '':
 						await message.channel.send(embed=discord.Embed(colour=discord.Colour(0x00ffff), description=result))
 
-client.run(discordbottoken.token)
+@bot.command(
+	help='Deletes messages of the specified channel. If no channel is specified it will purge the channel the command was issued in.\nUsage: "!fobot purge <channelname> <amount of messages to be deleted>"',
+        brief='Deletes all messages of the specified channel.'
+)
+async def purge(ctx, channel_name=None, number=100):
+    user = ctx.message.author
+    if not discord.utils.get(user.roles, name="Admins") and not discord.utils.get(user.roles, name="Liquipedia Staff"):
+            await ctx.send('You do not have permission to use this command.')
+    else:
+        if channel_name == None: channel = ctx.channel
+        else: channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+        if channel == None: await ctx.send('Invalid channel.')
+        await channel.purge(limit=number)
+
+@bot.command(
+	help='Creates a temp. private channel and invites the specified user to it.\nUsage: "!fobot create <user>"',
+        brief='Creates a temp. private channel and invites the specified user to it.'
+)
+async def create(ctx, member: discord.Member):
+    user = ctx.message.author
+    if not discord.utils.get(user.roles, name="Admins") and not discord.utils.get(user.roles, name="Liquipedia Staff"):
+            await ctx.send('You do not have permission to use this command.')
+    else:
+        guild = ctx.message.guild
+        admin_role = discord.utils.get(guild.roles, name="Admins")
+        private_role = discord.utils.get(guild.roles, name="Private Chat")
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True),
+            admin_role: discord.PermissionOverwrite(read_messages=True),
+            member: discord.PermissionOverwrite(read_messages=True)
+        }
+        await guild.create_text_channel('temp_' + member.name, overwrites=overwrites, category=bot.get_channel(int(discordbottoken.privcat)))
+        await ctx.message.delete()
+
+@bot.command(
+	help='Copies the specified channel (up to 10,000 msgs) to "Private_chat_log". If no channel is specified it will copy the channel the command was issued in. Only works in channels that start with "temp_".\nUsage: "!fobot copy <channelname>"',
+        brief='Copies the specified channel to "Private_chat_log".'
+)
+async def copy(ctx, channel_name=None):
+    user = ctx.message.author
+    if not discord.utils.get(user.roles, name="Admins") and not discord.utils.get(user.roles, name="Liquipedia Staff"):
+            await ctx.send('You do not have permission to use this command.')
+    else:
+        if channel_name == None: channel = ctx.channel
+        else: channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+        if channel == None or not channel.name.startswith('temp_'): await ctx.send('Invalid channel.')
+        else:
+            epmty_array = []
+            logtarget = bot.get_channel(discordbottoken.logtarget)
+            async for message in channel.history(limit=10000, oldest_first='true'):
+                time = message.created_at
+                embed = discord.Embed(
+                    title=message.author.display_name + ' on ' + str(time)[:-7] + ' UTC:',
+                    color=discord.Color.blue(),
+                    description=message.content
+                    )
+                await logtarget.send(embed=embed)
+                if message.attachments != epmty_array:
+                    files = message.attachments
+                    for file in files:
+                        await logtarget.send(file.url)
+
+@bot.command(
+	help='Copies the specified channel to "Private_chat_log" and deletes it thereafter. If no channel is specified it defaults to the channel the command was issued in. Only works in channels that start with "temp_".\nUsage: "!fobot kill_channel <channelname>"',
+        brief='Copies the specified channel to "Private_chat_log" and deletes it thereafter.'
+)
+async def kill_channel(ctx, channel_name=None):
+    user = ctx.message.author
+    if not discord.utils.get(user.roles, name="Admins") and not discord.utils.get(user.roles, name="Liquipedia Staff"):
+            await ctx.send('You do not have permission to use this command.')
+    else:
+        if channel_name == None: channel = ctx.channel
+        else: channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+        if channel == None or not channel.name.startswith('temp_'): await ctx.send('Invalid channel.')
+        else:
+            epmty_array = []
+            logtarget = bot.get_channel(discordbottoken.logtarget)
+            async for message in channel.history(limit=10000, oldest_first='true'):
+                time = message.created_at
+                embed = discord.Embed(
+                    title=message.author.name + ' on ' + str(time)[:-7] + ' UTC:',
+                    color=discord.Color.blue(),
+                    description=message.content
+                    )
+                await logtarget.send(embed=embed)
+                if message.attachments != epmty_array:
+                    files = message.attachments
+                    for file in files:
+                        await logtarget.send(file.url)
+                else:None
+                        
+            await channel.delete()
+
+#@create.error
+async def create_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Invalid username.')
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify a username.')
+
+bot.run(discordbottoken.token)
