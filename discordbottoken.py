e1ecb988fdb619af8eb1fc19ca1cb21d70203e46
hjpalpha <75081997+hjpalpha@users.noreply.github.com>
2020-11-28 10:48:08 +0100

    Update discordbottoken.py

--- discordbottoken.py
+++ discordbottoken.py
@@ -1,2 +1,4 @@
 token = ''
 apikey = ''
+logtarget = #here the channel id of the channel we want the private channel logs to be stored in
+privcat = 360564294401916929 #this is the id of the "Private Channels" Category
