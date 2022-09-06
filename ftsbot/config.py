afb9955d038aac083a2949024942e47a29dd3eb7
FO-nTTaX <fo.nttax@yahoo.de>
2022-09-06 03:19:33 +0200

    remove dev config

--- ftsbot/config.py
+++ ftsbot/config.py
@@ -8,6 +8,6 @@ logtarget = 339287109988909057 # Here the channel id of the channel we want the 
 privcat = 360564294401916929 # This is the id of the "Private Channels" Category
 reporttarget = 191894474094280704 # This is the id of the channel reports go to
 
-logtarget = 870842216124387339
-privcat = 870838820856270899
-reporttarget = 273820818809815041
+# logtarget = 870842216124387339
+# privcat = 870838820856270899
+# reporttarget = 273820818809815041
